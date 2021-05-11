d9d0f9398ce2557cf129502982831841f6a2f0f1
ayrton <adriancdma@gmail.com>
2021-05-10 21:44:45 -0300

    js

---
+++ script.js
@@ -0,0 +1,8 @@
+const circle = document.querySelector('#circle');
+
+document.addEventListener('DOMContentLoaded',ocultarSpiner)
+
+function ocultarSpiner(){
+    console.log(circle)
+    circle.classList.add('hidden')
+}
